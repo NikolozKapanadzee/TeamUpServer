5104523f1ad2ca21760d5b998449e74e7b21c2af
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-07-25 19:15:38 +0400

    Feat: Google OAuth

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-google-oauth2';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+      scope: ['email', 'profile'],
+    });
+  }
+  validate(accessToken, refreshToken, data, done) {
+    console.log(accessToken, 'accessToken');
+    console.log(refreshToken, 'refreshToken');
+    console.log(data, 'data');
+    done(null, data);
+  }
+}
